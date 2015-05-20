0b4f6aa54b53fddae1df2b90f49b6cffea4eb525
javier-lopez <javier.lopez@bq.com>
2015-05-20 18:06:44 +0200

    feat(js): add transitionEnd() and fn.emulateTransitionEnd() functions
    Change-Id: Iac0d200d441c479c7014d063b6d3bcae602a6cf9

---
+++ src/js/transition.js
@@ -0,0 +1,56 @@
+/* global define, module, require */
+
+'use strict';
+
+(function(factory, root) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory); // AMD
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('jquery')); // Node
+    } else {
+        factory(root.$); // Browser global
+    }
+})
+(function($) {
+
+    var transitionEnd = function() {
+
+        var el = document.createElement('transition')
+
+        var transEndEventNames = {
+            WebkitTransition: 'webkitTransitionEnd',
+            MozTransition: 'transitionend',
+            OTransition: 'oTransitionEnd otransitionend',
+            transition: 'transitionend'
+        }
+
+        for (var name in transEndEventNames) {
+            if (el.style[name] !== undefined) {
+                return {
+                    end: transEndEventNames[name]
+                }
+            }
+        }
+
+    }
+
+    // http://blog.alexmaccaw.com/css-transitions
+    $.fn.emulateTransitionEnd = function(duration) {
+
+        var called = false,
+            $el = this;
+
+        $(this).one('penguinTransitionEnd', function() {
+            called = true
+        });
+
+        var callback = function() {
+            if (!called) $($el).trigger('penguinTransitionEnd')
+        };
+
+        setTimeout(callback, duration)
+
+        return this
+    }
+
+}, window);
